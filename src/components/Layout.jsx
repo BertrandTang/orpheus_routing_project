2646198c7267968aa7011139895846994b10fc58
Bertrand <bertrand.tang.dev@gmail.com>
2025-07-21 14:34:19 +0200

    add as container to main in layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import Container from 'react-bootstrap/Container';
 import { Outlet } from "react-router";
 
 export default function Layout() {
   return (
     <>
       <Header />
-      <main>
+      <main as={Container}>
         <Outlet />
       </main>
       <Footer />
