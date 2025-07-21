d4cee53a42068448b52638cb5f1c26c0937dc271
Bertrand <bertrand.tang.dev@gmail.com>
2025-07-21 16:40:07 +0200

    activity 7

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,34 +1,45 @@
 import Nav from "react-bootstrap/Nav";
-import { NavLink } from "react-router";
-import '../assets/css/Header.css'
+import { NavLink, useMatch } from "react-router";
+import "../assets/css/Header.css";
 
 export default function Header() {
+  const match = useMatch("/category/:query");
+
   return (
     <header>
-      <Nav className="justify-content-end bg-light">
+      <Nav className="bg-light d-flex justify-content-between">
         <Nav.Item>
-          <Nav.Link as={NavLink} to="/" className="text-secondary" end>
-            Accueil
-          </Nav.Link>
+          {match ? (
+            <p className="mb-0 py-2 ps-3">Catégorie : {match.params.query}</p>
+          ) : (
+            <p className="mb-0 py-2 ps-3">Bienvenue sur le site</p>
+          )}
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            as={NavLink}
-            to="/category/vetements"
-            className="text-secondary"
-          >
-            Vêtements
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link
-            as={NavLink}
-            to="/category/chaussures"
-            className="text-secondary"
-          >
-            Chaussures
-          </Nav.Link>
-        </Nav.Item>
+        <div className="d-flex">
+          <Nav.Item>
+            <Nav.Link as={NavLink} to="/" className="text-secondary" end>
+              Accueil
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              as={NavLink}
+              to="/category/vetements"
+              className="text-secondary"
+            >
+              Vêtements
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link
+              as={NavLink}
+              to="/category/chaussures"
+              className="text-secondary"
+            >
+              Chaussures
+            </Nav.Link>
+          </Nav.Item>
+        </div>
       </Nav>
     </header>
   );
