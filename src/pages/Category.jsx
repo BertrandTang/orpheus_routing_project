28423a2a1d1de738b0425fd4d9b5bbaafe8e2865
Bertrand <bertrand.tang.dev@gmail.com>
2025-07-21 17:02:31 +0200

    activity 8

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,19 @@
 import { useParams } from "react-router";
+import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router';
 
 export default function Category() {
   const { category } = useParams();
+  const navigate = useNavigate();
+
+  function backToHomePage() {
+    navigate('/');
+  }
+
   return (
     <div>
       <h1>{category}</h1>
+      <Button variant="primary" onClick={backToHomePage}>Retour à la page d'accueil</Button>
     </div>
   );
 }
